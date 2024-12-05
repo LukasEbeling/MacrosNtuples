a3315772945c08b74dcafac55ef62ba0ea1f0c8c
LukasEbeling <lukas.ebeling@uni-hamburg.de>
2024-12-05 14:16:46 +0100

    executed cmd printed to output file

--- automation/wrapper.py
+++ automation/wrapper.py
@@ -3,16 +3,17 @@
 import argparse
 import os
 
-# parse arguments
-parser = argparse.ArgumentParser(description="wrapper running script on htcondor")
+automation_path = '/afs/cern.ch/user/l/lebeling/MacrosNtuples/automation'
 
-# Add an argument that accepts multiple values
+# parse commands to be executed as arguments
+parser = argparse.ArgumentParser(description="wrapper running script on htcondor")
 parser.add_argument('cmd', nargs='+', type=str, help='commands to be executed')
 args = parser.parse_args()
 
 concatenated_cmd = ' '.join(args.cmd)
 concatenated_cmd = concatenated_cmd.replace("___", " ")
 #concatenated_cmd = 'tar -xzf MacrosNtuples.tar.gz; cd MacrosNtuples/l1macros; ' + concatenated_cmd
-concatenated_cmd = f'cd /afs/cern.ch/user/l/lebeling/MacrosNtuples/automation; ' + concatenated_cmd
+concatenated_cmd = f'cd {automation_path}; ' + concatenated_cmd
 
+print('command to be executed: ' + concatenated_cmd)
 os.system(concatenated_cmd)

a848e40319f5d6467c1160e0ac0400142d917228
LukasEbeling <lukas.ebeling@uni-hamburg.de>
2024-12-05 14:17:22 +0100

    new abort condition for hadd

--- automation/utils.py
+++ automation/utils.py
@@ -1,14 +1,12 @@
-import os, subprocess, uproot
+import os, subprocess, uproot, argparse
 import pandas as pd
 
-script_dir = os.path.join(os.getcwd(), "../l1macros")
+#dqm_prefix = '/eos/cms/store/group/dpg_trigger/comm_trigger/L1Trigger/cmsl1dpg/www/DQM/T0PromptNanoMonit'
 dqm_prefix = "/eos/user/l/lebeling/www/DQM" 
-out_prefix = "/eos/user/l/lebeling/www/DQM" 
 tier0 = "/eos/cms/tier0/store/data"
 
 
 def run_command(cmd, log_file = "log.txt"):
-    os.chdir(script_dir)
     with open(log_file, "a") as f:
         subprocess.run(cmd, shell=True, stdout=f, stderr=f) 
 
@@ -24,7 +22,7 @@ def parse_file(fname):
         label = ''.join([char for char in dataset if not char.isdigit()])
         
         #return f"{year}/{label}/{era}/{run}/{base_fname}"
-        return f"{label}/{era}/{dataset}/{reco_version}/{run}/{base_fname}"
+        return f"/{label}/{era}/{dataset}/{reco_version}/{run}/{base_fname}"
 
 
 def write_queue(script, infile = "", outdir = ""):
@@ -34,6 +32,7 @@ def write_queue(script, infile = "", outdir = ""):
         f.write(cmd + "\n")
 
 
+# return weeks as dict with runnum as key -> weeks[runx] = 42
 def get_weeks():
     oms_path = "/eos/cms/store/group/tsg/STEAM/OMSRateNtuple/2024/physics.root"
     with uproot.open(oms_path) as f:
@@ -43,12 +42,19 @@ def get_weeks():
         )
     df['date'] = pd.to_datetime(df[['year', 'month', 'day']])
     df['week'] = df['date'].dt.isocalendar().week
-    
-    result_dict = {}
-    for _, row in df.iterrows():
-        result_dict[row['run']] = row['week']
 
-    return result_dict
+    min_run = df.groupby('week')['run'].min()
+    max_run = df.groupby('week')['run'].max()
+    
+    weeks = {}
+    for _, row in df.iterrows():
+        w = row['week']
+        r = row['run']
+        min_r = min_run[w]
+        max_r = max_run[w]
+        weeks[r] = f'Week{w}_{min_r}-{max_r}'
+
+    return weeks
 
 
 def load_filelist(path):
@@ -64,18 +70,32 @@ def save_filelist(path, files):
             f.write(file + "\n")
 
 
-def hadd(target, files, local):
+def hadd(target, files, htcondor = False):
     os.makedirs(os.path.dirname(target), exist_ok=True)
 
-    filelist = load_filelist(target.replace('root','txt'))
+    # filelist = load_filelist(target.replace('root','txt'))
+    # if set(filelist) == set(files): 
+    #     print('skipping ' + target, "already hadded")
+    #     return
+    # save_filelist(target.replace('root','txt'), files)
 
-    if set(filelist) == set(files): 
-        print('skipping ' + target, "already hadded")
-        return
-
-    save_filelist(target.replace('root','txt'), files)
+    # abort if merged file already exists, and it is newer than all base files
+    if os.path.exists(target):
+        target_time = os.path.getctime(target)
+        files_time = max([os.path.getctime(file) for file in files])
+        if target_time > files_time:
+            print(f"skipping {target} - newer than all base files")
+            return
 
     print(f"Hadding files with target {target}")
     cmd = f'hadd -f {target} ' + ' '.join(files)
-    if local: run_command(cmd, os.path.dirname(target)+"/log.txt")
-    else: write_queue(cmd)
+    if htcondor: write_queue(cmd)
+    else: run_command(cmd, os.path.dirname(target)+"/log.txt")
+
+
+def htcondor_flag():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--htcondor', action='store_true', help='run on ht condor')
+    args = parser.parse_args()
+    if args.htcondor: os.system('rm -rf queue.txt')
+    return args.htcondor
